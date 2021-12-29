81863b06ddfac867add62d668500f9875e0651fc
anqurvanillapy <anqurvanillapy@gmail.com>
2021-12-29 18:45:38 +0800

    Nits

--- examples/main.go
+++ examples/main.go
@@ -9,6 +9,7 @@ import (
 type HelloService struct{}
 
 func (HelloService) SayHi(g hello.Greeting) *hello.Reply {
+	fmt.Printf("%+v\n", g)
 	if i, ok := g.(*hello.SelfIntro); ok {
 		return &hello.Reply{Message: fmt.Sprintf("Hi, %s!", i.Name)}
 	}
